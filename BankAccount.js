b27ec609426ff7a50085b6689b24e4bc9ec9e736
FarhathH <farhathh0@gmail.com>
2023-10-31 10:52:10 +0000

    Adding methods
    :)

--- BankAccount.js
+++ BankAccount.js
@@ -1,27 +1,34 @@
 //Created a class
 class BankAccount{
 
-    constructor(hol,bal, sort, accNo){
+    constructor(hol, bal, accNo, sort){
         
         this.accountHolder = hol
         this.balance = bal
         this.sortCode = sort
         this.accountNumber = accNo
+        
 
+    }
+
+    getDescription(){ //this method is used for return all the values
+        return `${this.accountHolder} has £${this.balance} in their bank account. The account number is ${this.accountNumber} and the sort code is ${this.sortCode}.`
     }
    
     
-    modifyBalance(num1){
+    modifyBalance(num1){ //increases the balance based on the deposit
         return this.balance += num1
     }
   
 }
 
+//new object with values in regards to the parameters from the constructor
+let acc1 = new BankAccount("Sally Face",393425, 99857849 ,545657)
 
-let acc1 = new BankAccount("Sally Face",393425, 545657, 99857849)
 
-console.log(acc1.balance)
-acc1.modifyBalance(100)
-console.log(acc1.balance)
+console.log(acc1.balance) //prints out balance from acc1 object
+acc1.modifyBalance(100) //using function to increment balance
+console.log(acc1.balance) //prints out new balance
+console.log(acc1.accountHolder) //prints the name of the account holder
 
-console.log(acc1.accountHolder)
+console.log(acc1.getDescription()) //prints out all the info of the bank account
