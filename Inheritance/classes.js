5d7514cfe4349c69a5c6a0ade5c21dc8fdfb2060
FarhathH <farhathh0@gmail.com>
2023-11-01 10:02:17 +0000

    working on inheritance for classes

--- Inheritance/classes.js
+++ Inheritance/classes.js
@@ -1,4 +1,6 @@
 //recaping classes
+
+//Mother class
 class Animal{
     constructor(name, age){
         this.name = name
@@ -6,8 +8,34 @@ class Animal{
 
     }
 
-    getInfo(){
-        
+   
+
+}
+
+//Child class
+class Dog extends Animal{
+    constructor(name, age){
+        super(name,age) //inherit properties from mother class
+    }
+
+    makeSound(){
+        console.log("Woof")
+    }
+
+    getAgeInDogyears(){
+        return this.age * 7
     }
 
 }
+
+//new object drom dog class
+let dog1 = new Dog("Benny", 5)
+
+
+console.log(dog1.name)
+dog1.makeSound()
+dog1.age
+dog1.getAgeInDogyears()
+
+
+
